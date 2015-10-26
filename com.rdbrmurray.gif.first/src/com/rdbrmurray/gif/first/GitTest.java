4710ba5af4b9bf8de30870fe0f760f5b2accbd80
rdbrmurray <rdbrmurray@msn.com>
2015-10-26 07:39:29 -0400

    Change output message
    I did this to show how to commit changes

--- com.rdbrmurray.gif.first/src/com/rdbrmurray/gif/first/GitTest.java
+++ com.rdbrmurray.gif.first/src/com/rdbrmurray/gif/first/GitTest.java
@@ -13,7 +13,7 @@ public class GitTest {
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println("Git is cool");
+	System.out.println("Git is really cool");
     }
 
 }
