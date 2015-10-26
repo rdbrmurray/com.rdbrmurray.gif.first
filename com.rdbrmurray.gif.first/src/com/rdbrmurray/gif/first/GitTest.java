6856bc2db1b5d0ddd9c23d83b16235365e575324
rdbrmurray <rdbrmurray@msn.com>
2015-10-26 07:34:34 -0400

    Initial commit of git.first

---
+++ com.rdbrmurray.gif.first/src/com/rdbrmurray/gif/first/GitTest.java
@@ -0,0 +1,19 @@
+/* GitTest.java
+ * Created on Oct 26, 2015 
+ */
+package com.rdbrmurray.gif.first;
+
+/**
+ * @author rdbrmurray
+ *
+ */
+public class GitTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println("Git is cool");
+    }
+
+}
